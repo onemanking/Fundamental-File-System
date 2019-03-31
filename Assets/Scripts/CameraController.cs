25ec76051632d0c70856fe0e76398b682ab00e52
Rachen Tanimkarn <onemanking@hotmail.com>
2019-03-31 16:48:08 +0700

    deleted unused assets, added System.Windows.Forms.dll to plugins folder, added CameraController and FileManager scripts

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour {
+    public Transform target;
+
+    private float xOffset = 20;
+
+    [Header ("ScreenShot")]
+    [SerializeField]
+    private int captureWidth = 2048;
+    [SerializeField]
+    private int captureHeight = 2048;
+    [SerializeField]
+    private RawImage rawImage;
+
+    [Header ("File Browser")]
+    public FileManager fileManager;
+
+    private Camera camera;
+    private Texture2D currentImage;
+    private bool takeScreenShot;
+
+    private string path;
+
+    // Start is called before the first frame update
+    void Start () {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (target) {
+            Quaternion OriginalRot = transform.rotation;
+            transform.LookAt (target);
+            Quaternion NewRot = Quaternion.Euler (transform.rotation.eulerAngles.x - xOffset, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+            transform.rotation = OriginalRot;
+            transform.rotation = Quaternion.Lerp (transform.rotation, NewRot, 2 * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown (KeyCode.Return)) {
+            TakeScreenShot ();
+        } else if (Input.GetKeyDown (KeyCode.Space)) {
+            StartCoroutine (OpenFileBrowser ());
+        } else if (Input.GetKeyDown (KeyCode.O)) {
+            rawImage.texture = fileManager.LoadPNG (path);
+        }
+    }
+
+    void OnPostRender () {
+        if (takeScreenShot) {
+            takeScreenShot = false;
+            RenderTexture renderTexture = camera.targetTexture;
+            Texture2D texture = new Texture2D (renderTexture.width, renderTexture.height, TextureFormat.ARGB32, false);
+            Rect rect = new Rect (0, 0, renderTexture.width, renderTexture.height);
+            texture.ReadPixels (rect, 0, 0);
+
+            rawImage.texture = fileManager.SavePNG (texture.EncodeToPNG ());
+
+            RenderTexture.ReleaseTemporary (renderTexture);
+            camera.targetTexture = null;
+        }
+    }
+
+    void TakeScreenShot () {
+        camera.targetTexture = RenderTexture.GetTemporary (captureWidth, captureHeight);
+        takeScreenShot = true;
+    }
+
+    public IEnumerator OpenFileBrowser () {
+        // PREVENT SPACEBAR INPUT CLOSE DIALOG
+        yield return new WaitForSecondsRealtime (0.1f);
+        fileManager.FolderBrowserPanel ("Change Save location");
+    }
+}
---
+++ Assets/Scripts/FileManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Windows.Forms;
+using UnityEngine;
+
+public class FileManager : MonoBehaviour {
+
+    [HideInInspector]
+    public string directory;
+
+    public bool onBrowse;
+
+    void Start () {
+        directory = UnityEngine.Application.persistentDataPath;
+    }
+
+    public string SaveFilePanel (string title, string defaultName) {
+        SaveFileDialog saveFileDialog = new SaveFileDialog ();
+
+        saveFileDialog.Title = title;
+
+        var finalFilename = "";
+
+        if (!string.IsNullOrEmpty (directory)) {
+            finalFilename = GetDirectoryPath (directory);
+        }
+
+        if (!string.IsNullOrEmpty (defaultName)) {
+            finalFilename += defaultName;
+        }
+
+        saveFileDialog.FileName = finalFilename;
+
+        saveFileDialog.DefaultExt = ".png";
+        saveFileDialog.Filter = "Image (*.png)|*.png";
+        var result = saveFileDialog.ShowDialog ();
+        var filename = result == DialogResult.OK ? saveFileDialog.FileName : "";
+        saveFileDialog.Dispose ();
+        return filename;
+    }
+
+    public void FolderBrowserPanel (string description) {
+        FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog ();
+        folderBrowserDialog.Description = description;
+        var result = folderBrowserDialog.ShowDialog ();
+        var path = result == DialogResult.OK && !string.IsNullOrEmpty (folderBrowserDialog.SelectedPath) ? folderBrowserDialog.SelectedPath : directory;
+        folderBrowserDialog.Dispose ();
+        directory = path;
+    }
+
+    private string GetDirectoryPath (string directory) {
+        var directoryPath = Path.GetFullPath (directory);
+        if (!directoryPath.EndsWith ("\\")) {
+            directoryPath += "\\";
+        }
+        if (Path.GetPathRoot (directoryPath) == directoryPath) {
+            return directory;
+        }
+        return Path.GetDirectoryName (directoryPath) + Path.DirectorySeparatorChar;
+    }
+
+    public Texture2D SavePNG (byte[] bytes) {
+        string path = SaveFilePanel ("Save", "ScreenShot.png");
+        if (!string.IsNullOrEmpty (path)) {
+            System.IO.File.WriteAllBytes (path, bytes);
+        }
+        return LoadPNG (path);
+    }
+
+    public Texture2D LoadPNG (string filePath) {
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists (filePath)) {
+            fileData = File.ReadAllBytes (filePath);
+            tex = new Texture2D (2, 2);
+            tex.LoadImage (fileData);
+        }
+        return tex;
+    }
+}
